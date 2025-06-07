a2332bac8375d2a339e0559424ea5e2d2d9f0858
Archit Batra <dev@architbatra.in>
2025-06-07 21:51:44 +0530

    added goroutines

--- filemanager/filemanager.go
+++ filemanager/filemanager.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"os"
+	"time"
 )
 
 type FileManager struct {
@@ -19,6 +20,8 @@ func (fm FileManager) ReadLines() ([]string, error) {
 		return nil, errors.New("Failed to open file.")
 	}
 
+	defer file.Close()
+
 	scanner := bufio.NewScanner(file)
 
 	var lines []string
@@ -30,11 +33,9 @@ func (fm FileManager) ReadLines() ([]string, error) {
 	err = scanner.Err()
 
 	if err != nil {
-		file.Close()
 		return nil, errors.New("Failed to read line in file.")
 	}
 
-	file.Close()
 	return lines, nil
 }
 
@@ -45,15 +46,17 @@ func (fm FileManager) WriteResult(data interface{}) error {
 		return errors.New("Failed to create file.")
 	}
 
+	defer file.Close()
+
+	time.Sleep(3 * time.Second)
+
 	encoder := json.NewEncoder(file)
 	err = encoder.Encode(data)
 
 	if err != nil {
-		file.Close()
-		return errors.New("Faild to convert data to JSON.")
+		return errors.New("Failed to convert data to JSON.")
 	}
 
-	file.Close()
 	return nil
 }
 
